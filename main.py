8ec4f3518d3e32b1e6b60a0688f1504245767d74
Elaina-Quinn <elaina.riggert@gmail.com>
2024-12-29 23:14:56 -0800

    added an fps limit to lower resource usage

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # fps variables
+    clock = pygame.time.Clock()
+    dt = 0
+
     # infinite while loop to run asteroids continuously
     while True:
         # exits the program when window is closed
@@ -14,6 +18,11 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        # fps limit
+        dt = (clock.tick(60)) / 1000
+
+
 
 if __name__ == "__main__":
     main()
