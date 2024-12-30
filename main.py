249a1f633311e99453fb877c1892cb0d3a20af00
Elaina-Quinn <elaina.riggert@gmail.com>
2024-12-29 22:55:35 -0800

    made a window open and stay open, and have the closing of the window also exit the program

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import *
 
 def main():
-    print ("Starting asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # infinite while loop to run asteroids continuously
+    while True:
+        # exits the program when window is closed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
