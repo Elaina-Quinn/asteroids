4d3e6f27686d1db05651b2cf237797960015aa68
Elaina-Quinn <elaina.riggert@gmail.com>
2024-12-30 21:48:37 -0800

    added asteroid and asteroidfield classes and containers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,11 +15,15 @@ def main():
     # creating groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
-    # player initilization
+    # objuect initilization
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    AsteroidField()
 
     # infinite while loop to run asteroids continuously
     while True:
